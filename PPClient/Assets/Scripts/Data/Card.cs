64761890a6b18523bc660ff66dc798646b97dbee
Soomin Oh <hilariousbananana@gmail.com>
2025-02-22 00:22:16 +0900

    [Update] Card constructor
    카드 생성자 추가

--- PPClient/Assets/Scripts/Data/Card.cs
+++ PPClient/Assets/Scripts/Data/Card.cs
@@ -28,6 +28,11 @@ public class Card
 	public Suit Suit { get; }
 	public Rank Rank { get; }
 
+	public Card( Suit suit, Rank rank )
+	{
+		this.Suit = suit;
+		this.Rank = rank;
+	}
 	public override string ToString()
 	{
 		return $"{Rank} of {Suit}";
