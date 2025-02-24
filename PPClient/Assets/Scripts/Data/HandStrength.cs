c3afda89449474259e4279919979cafbf058abdc
Soomin Oh <hilariousbananana@gmail.com>
2025-02-24 18:14:45 +0900

    [Add] HandStrength class

---
+++ PPClient/Assets/Scripts/Data/HandStrength.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandStrength
+{
+	public HandRank   Rank;
+	public Rank       MainCard;
+	public List<Card> Kickers;
+}
