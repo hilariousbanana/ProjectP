72f1ce08c5a48681ced6a319acdca94540ad440d
Soomin Oh <hilariousbananana@gmail.com>
2025-02-26 18:37:43 +0900

    [Add] GameManager

---
+++ PPClient/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private List<Card> deck;
+	private List<Hand> hands;
+	private Board      board;
+
+	public void Initialize()
+	{
+
+	}
+
+	private void CrateDeck()
+	{
+
+	}
+	public void ShuffleDeck()
+	{
+
+	}
+
+	public void DealHands()
+	{
+
+	}
+	public void DealBoard()
+	{
+
+	}
+
+	public void RevealFlop()
+	{
+
+	}
+	public void RevealTurn()
+	{
+
+	}
+	public void RevealRiver()
+	{
+
+	}
+
+}
